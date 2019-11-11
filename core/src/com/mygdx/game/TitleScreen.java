d9d91c27818665531126d5ddf3bc6ca547015262
YoungRichi <34274756+YoungRichi@users.noreply.github.com>
2019-11-10 23:38:47 -0500

    added UI skin

--- core/src/com/mygdx/game/TitleScreen.java
+++ core/src/com/mygdx/game/TitleScreen.java
@@ -1,23 +1,38 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 
 public class TitleScreen extends ScreenBeta {
 
     ActorBeta background;
-
     ActorBeta tapToPlay;
+    Skin arcade;
+    Label tap;
 
     static float WIDTH = Gdx.graphics.getWidth();
     static float HEIGHT = Gdx.graphics.getHeight();
+
     @Override
     public void initialize() {
+        arcade = new Skin(Gdx.files.internal("arcade/skin/arcade-ui.json"));
+
+
         background = new ActorBeta(0, 0, mainStage);
         background.loadTexture("TitleScreen.png");
         background.setSize(WIDTH,HEIGHT);
 
         tapToPlay =  new ActorBeta(1500, 500, mainStage);
         tapToPlay.loadTexture("TapMe.png");
+
+        tap = new Label("TAP!!!", arcade);
+        tap.setPosition(WIDTH /2, HEIGHT *7/8);
+        tap.setScale(2);
+        tap.setFontScale(3.f);
+        //tap.setWrap(true);
+
+        mainStage.addActor(tap);
 
 
 
